868d332bfd1cf30e719064b364ace968798ba5f7
Manish <moneyyiiss@gmail.com>
2024-06-01 21:27:43 +0530

    product service

---
+++ src/test/java/com/techview365/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techview365.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
